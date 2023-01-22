7964a0f675b8580998e0b5647813e8fbeac057b6
atmoner <contact.atmoner@gmail.com>
2023-01-22 13:50:54 +0100

    Add example with chain-registry

--- example.js
+++ example.js
@@ -3,6 +3,7 @@ let { bech32 } = require('bech32')
 let decode = bech32.decode('bcna13jawsn574rf3f0u5rhu7e8n6sayx5gkwgusz73')
 
 let addrArray = []
+
 for (const key in chains) {
   addrArray.push([
     chains[key].chain_name, 
